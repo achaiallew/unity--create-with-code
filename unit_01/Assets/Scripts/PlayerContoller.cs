1f2a300e7a3df64614e6844bd5d55f2cdadcca55
Achaia Beukes <achaiallew@gmail.com>
2026-06-29 22:15:38 +0200

    Minor Fixes

--- unit_01/Assets/Scripts/PlayerContoller.cs
+++ unit_01/Assets/Scripts/PlayerContoller.cs
@@ -6,19 +6,19 @@ public class PlayerContoller : MonoBehaviour
     // Declare Variables
     public float speed = 10;
     public float turnSpeed = 3;
-    public InputAction MoveAction;
+    public InputAction moveAction;
     public Vector2 moveInput;
 
     void Start()
     {
         // Enable Movement Actions
-        MoveAction.Enable();
+        moveAction.Enable();
     }
 
     void Update()
     {
         // Read Movement Input
-        moveInput = MoveAction.ReadValue<Vector2>();
+        moveInput = moveAction.ReadValue<Vector2>();
         // Move Player Forward
         transform.Translate(Vector3.forward*Time.deltaTime*speed*moveInput.y);
         transform.Rotate(Vector3.up*Time.deltaTime*turnSpeed*moveInput.x);
